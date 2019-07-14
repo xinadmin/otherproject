70d9b0c04332185b031ffdb651d6a676a21e3249
xin <1197196234@qq.com>
2019-07-15 02:10:07 +0800

    第一次

---
+++ pc_console/src/views/user/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <router-link to="/user/Usermessage">
+            <button>跳转</button>
+        </router-link>
+
+    </div>
+
+</template>
+
+<script>
+    import { getuserlist } from '@/api/user'
+    export default {
+        data() {
+            list: null
+        },
+        created:{
+            // this.fetchData()
+        },
+        methods: {
+            fetchData() {
+                // this.listLoading = true
+                getuserlist().then(response => {
+                    console.log(response)
+                    // this.list = response.data.items
+                    // this.listLoading = false
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
